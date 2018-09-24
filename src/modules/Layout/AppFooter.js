2b76d5c6fe2a5edde67139ecb46ec9b05b60e94a
Minh Nguyen <s4355206@student.uq.edu.au>
2018-09-24 23:08:44 +1000

    redo footer

--- src/modules/Layout/AppFooter.js
+++ src/modules/Layout/AppFooter.js
@@ -4,47 +4,37 @@ import { Grid, Container, Divider, Header } from 'semantic-ui-react'
 export default class AppFooter extends React.Component {
 
     render() {
+
+        /* Social Links to Developer */
+        const clickers = [
+            {
+                href: "https://www.facebook.com/minh.nguyen.14661",
+                icon: 'facebook square icon huge'
+            },
+            {
+                href: "https://github.com/minhhho2",
+                icon: 'git square icon huge'
+            },
+            {
+                href: "https://www.linkedin.com",
+                icon: 'linkedin square icon huge'
+            }
+        ];
+
         return (
-            <div className='text-center'>
+            <div className='text-center'>=
 
-                <Container >
+                <Divider className='bg-white m-3' />
 
-                    {/*  Main footer content */}
-                    <Grid divided='vertically'>
-                        <Grid.Row columns={3} className='text-white text-left p-5'>
+                {/*  T&A footer content */}
+                <Container className='p-5'>
 
-                            <Grid.Column>
-                                <Header className='text-white' as='h2'> QAHES</Header>
-                                <p>123 Abc Street, 4075, Brisbane</p>
-                                <p>(+61) 400 000 000</p>
-                            </Grid.Column>
-
-                            <Grid.Column >
-                                <Header className='text-white' as='h2'> _ANOTHER COLUMN_</Header>
-                                <p>Content One</p>
-                                <p>Content Two</p>
-                            </Grid.Column>
-
-                            <Grid.Column >
-                                <Header className='text-white' as='h2'> COMPANY</Header>
-                                <p>About Us</p>
-                                <p>Contact Us</p>
-                            </Grid.Column>
-
-                        </Grid.Row>
-                    </Grid>
-                    <Divider className='bg-white' />
-
-                    {/*  T&A footer content */}
-                    <div className='text-center text-white p-5'>
-                        <span className='p-3'>Privacy</span>
-                        <span className='p-3'>Terms</span>
-                        <span className='p-3'>Sitemap</span>
-                    </div>
-
+                    <Header as='h3' className='inverted'> Minh D. Nguyen</Header>
+                    {clickers.map((click, index) => {
+                        return <a key={index} href={click.href}> <i className={click.icon}></i></a>
+                    })}
                 </Container>
             </div>
-
         )
     }
 }
